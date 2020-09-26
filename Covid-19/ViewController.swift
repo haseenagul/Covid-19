4d7907df4389c7966106999fdaf0e05508088ed3
haseena gul <haseenagul100@gmail.com>
2020-09-26 16:17:23 +0500

    Initial Commit

---
+++ Covid-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19
+//
+//  Created by Haseena Gul on 26/09/2020.
+//  Copyright © 2020 haseena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
